c61b5e0e2c58e48f83a8d1ce7f06b8985ac255ef
soapgu <ghost_gu@163.com>
2021-11-30 16:38:05 +0800

    add BluetoothLeScanner

--- app/src/main/java/com/soapgu/hellobluetooth/MainActivity.java
+++ app/src/main/java/com/soapgu/hellobluetooth/MainActivity.java
@@ -2,11 +2,15 @@ package com.soapgu.hellobluetooth;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,15 +18,19 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView existDeviceView;
     private TextView scanDeviceView;
+    private Button searchButton;
     private StringBuilder scanOutput;
+    private boolean isScan;
 
     BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    BluetoothLeScanner scanner;
 
     ActivityResultLauncher<Intent> mDiscoverability = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -31,6 +39,33 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText( getApplicationContext() , message , Toast.LENGTH_SHORT).show();
             });
 
+    ScanCallback mLeScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            if( result.getDevice().getName() != null )
+                scanDeviceView.setText(scanOutput.append(result.getDevice().getName())
+                        .append(  String.format( "(%s dBm)" , result.getRssi() ) )
+                        .append("\r\n"));
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+            scanOutput = new StringBuilder();
+            for( ScanResult scanResult : results ) {
+                scanDeviceView.setText(scanOutput.append(scanResult.getDevice().getName())
+                                                .append( scanResult.getRssi() )
+                                                .append("\r\n"));
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(receiver, filter);
+        scanner = bluetoothAdapter.getBluetoothLeScanner();
         this.initializeControls();
     }
 
@@ -50,7 +86,9 @@ public class MainActivity extends AppCompatActivity {
     private void initializeControls(){
         this.existDeviceView = this.findViewById(R.id.existDeviceView);
         this.scanDeviceView = this.findViewById(R.id.scanDeviceView);
-        this.findViewById(R.id.searchButton).setOnClickListener( v ->{
+
+        searchButton = this.findViewById(R.id.searchButton);
+        searchButton.setOnClickListener( v ->{
             if( bluetoothAdapter.isEnabled() ){
                 Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
                 if( !pairedDevices.isEmpty() ) {
@@ -60,10 +98,20 @@ public class MainActivity extends AppCompatActivity {
                     }
                     this.existDeviceView.setText(existOutput);
                 }
-                scanOutput = new StringBuilder();
-                bluetoothAdapter.startDiscovery();
+
+                //bluetoothAdapter.startDiscovery();
+                if( !isScan ) {
+                    scanOutput = new StringBuilder();
+                    scanner.startScan(mLeScanCallback);
+                }
+                else {
+                    scanner.stopScan(mLeScanCallback);
+                }
+                isScan = !isScan;
+                this.searchButton.setText( isScan ? "stop":"scan" );
             }
         } );
+
 
         this.findViewById(R.id.discoverabilityButton).setOnClickListener( v -> {
             Intent discoverableIntent =
